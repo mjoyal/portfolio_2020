c55dd193c691f3e115643be0279d5ccb7c0cd236
MacKenzie Joyal <mackenzie.joyal@gmail.com>
2020-12-10 20:23:00 -0800

    add resume download, add styled icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,16 @@ import "../styles/Header.scss";
 export default function Header (props) {
   return (
     <header>
-      <h2>Passionate full-stack web developer, 
-         <br></br>dedicated to learning && enthusiastic 
-         about intuitive design.</h2>
+      
+      <div>
+      <img className="arrow" src="/images/terminal-arrow.png" alt="cursor"/>
+       <h2>
+        Passionate full-stack web developer, 
+        dedicated to learning && enthusiastic 
+        about intuitive design. 
+       </h2>
+      <img className="cursor" src="/images/cursor.png" alt="cursor"/>
+      </div>
       <img src="images/computer-boy.png" alt="computer"/>
     </header>
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,8 +6,8 @@ export default function Nav (props) {
     <nav>
     <a href="/"><h1>MACKENZIE <span>JOYAL</span></h1></a>
     <ul>
-      <li><a href="/">download_resume</a></li>
-      <li><a href="/">endorsements</a></li>
+      <li><a href="documents/mackenzie-joyal-resume.pdf" target="_blank">download_resume</a></li>
+      {/* <li><a href="/">endorsements</a></li> */}
       <li><a href="/">contact</a></li>
     </ul>
     </nav>
