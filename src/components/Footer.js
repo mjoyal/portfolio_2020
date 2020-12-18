8a918e4b686ea199c59e5b5c5a06a731e5e04324
MacKenzie Joyal <mackenzie.joyal@gmail.com>
2020-12-17 18:12:29 -0800

    added copy to clipboard functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
 import "../styles/Footer.scss";
+import {useRef} from 'react';
 
 export default function Footer (props) {
+  const emailText = useRef(null); 
+
+  const copyEmail = function (e) {
+    emailText.current.select();
+    document.execCommand('copy'); 
+    props.showCopied();
+  };
+
   return (
     <footer>
+      <input ref={emailText} readOnly className="email-hide" type="text" value={'mackenzie.joyal@gmail.com'}/>
       <h2>say_hi()!</h2>
       <div>
         <a href={"https://github.com/mjoyal"} target="_blank">
@@ -11,7 +21,7 @@ export default function Footer (props) {
         <a href={"https://www.linkedin.com/in/mackenziejoyal/"} target="_blank">
         <img src="images/linkedin-white.png" alt="linkedin icon"/>
         </a>
-        <img src="images/email-white.png" alt="email icon" onClick={props.showCopied}/>
+        <img src="images/email-white.png" alt="email icon" onClick={copyEmail}/>
       </div>
     </footer>
   );
